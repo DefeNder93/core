7b5ae39c92b14146a0cc2a15d8377fcfe3a70bd7
DefeNder93 <alexdefender93@gmail.com>
2017-02-12 23:28:11 +0800

    Traffic Light example was changed

--- examples/states/trafficLight/TrafficLight.js
+++ examples/states/trafficLight/TrafficLight.js
@@ -1,3 +1,18 @@
 var TrafficLight = {
-    state: Core.state('Red', 'Yellow', 'Green')
+    state: Core.state('Red', 'Yellow', 'Green'),
+    __init: function() {
+        this.state.go('Red');
+    },
+    switchToRed: function() {
+        CatchEvent(TrafficLight.state.GoYellow);
+        setTimeout(function() { this.state.go('Red'); }.bind(TrafficLight), 1000);
+    },
+    switchToGreen: function() {
+        CatchEvent(TrafficLight.state.GoRed);
+        setTimeout(function() { this.state.go('Green'); }.bind(TrafficLight), 3000);
+    },
+    switchToYellow: function() {
+        CatchEvent(TrafficLight.state.GoGreen);
+        setTimeout(function() { this.state.go('Yellow'); }.bind(TrafficLight), 10000);
+    }
 };
--- examples/states/trafficLight/main.js
+++ examples/states/trafficLight/main.js
@@ -1,21 +1,2 @@
-Core.processGlobal();
 TrafficLight.state.value = 'Green'; // initial value
-
-function startChangingLights(light) {
-    console.log('TrafficLight: current light is ' + light);
-    TrafficLight.state.go(light);
-    setTimeout(function(){
-        switch(TrafficLight.state.value) {
-            case 'Red':
-                startChangingLights('Yellow');
-                break;
-            case 'Yellow':
-                startChangingLights('Green');
-                break;
-            case 'Green':
-                startChangingLights('Red');
-                break;
-        }
-    }, 3000);
-}
-startChangingLights('Red');
+Core.processGlobal();
