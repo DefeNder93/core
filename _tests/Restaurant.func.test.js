955dd53665811df4ffd1d493ec4fd7c5a59838e7
DefeNder93 <alexdefender93@gmail.com>
2017-03-26 18:49:42 +0300

    Restaurant test was fixed

--- _tests/Restaurant.func.test.js
+++ _tests/Restaurant.func.test.js
@@ -1,28 +1,28 @@
-// TODO fix it
-// describe("RestaurantSpec", function() {
-//     var include = require('./helpers/Utils').include;
-//
-//     include('Core.js');
-//     Core.registerEventPoint('DOM_Init'     , {log: false});
-//     include('examples/requests/restaurant/Cook.js');
-//     include('examples/requests/restaurant/Visitor.js');
-//
-//     beforeEach(function() {
-//         Core.processGlobal();
-//     });
-//
-//     it("Test restaurant", function(done) {
-//
-//         var John = createVisitor('John','Rolls');
-//         var Ada = createVisitor('Ada','Ramen');
-//
-//         Cook.putPlate({type: 'Ramen'});
-//         //Cook.putPlate({type: 'Sushi'});
-//         //Cook.putPlate({type: 'Rolls'});
-//
-//         setTimeout(function(){
-//             done();
-//         }, 7000);
-//     });
-//
-// });
+var createVisitor = require('../examples/requests/restaurant/Visitor.js');
+var assert = require('assert');
+describe("RestaurantSpec", function() {
+    context('main context', function(){
+        var include = require('./helpers/Utils').include;
+        include('Core.js');
+
+        Core.registerEventPoint('DOM_Init'     , {log: false});
+        include('examples/requests/restaurant/Cook.js');
+        
+        global.John = createVisitor('John','Rolls');
+        global.Ada = createVisitor('Ada','Ramen');
+        
+        beforeEach(function() {
+            Core.processGlobal();
+        });
+
+        it("Test Restaurant", function(done) {
+            Cook.putPlate({type: 'Ramen'});
+            Cook.putPlate({type: 'Sushi'});
+            Cook.putPlate({type: 'Rolls'});
+            assert.equal(John.eaten[0], 'Rolls');
+            assert.equal(Ada.eaten[0], 'Ramen');
+            done();
+        });
+    });
+});
+
--- examples/requests/restaurant/Visitor.js
+++ examples/requests/restaurant/Visitor.js
@@ -1,19 +1,23 @@
-function createVisitor(name, preferredType) {
+var createVisitor = function(name, preferredType) {
     return {
         preferredType: preferredType,
         name: name,
+        eaten: [],
         takePlate: function() {
             var _this = this;
             var plate = CatchRequest(Cook_putPlate);
-
             console.log(name + ": I see plateType " + plate.type);
 
             if (plate.type === _this.preferredType) {
                 return function(success, error) {
                     console.log(name + ": uhhmmmm! I've eaten the " + plate.type);
+                    _this.eaten.push(plate.type);
                     success();
                 }
             }
         }
     };
+};
+if(typeof require != 'undefined') {
+    module.exports = createVisitor;
 }
